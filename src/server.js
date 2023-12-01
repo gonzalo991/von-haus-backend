097580cd6ea27a623942c87c8f8e355a14aabdc0
Gonzalo Araya <gonxaraya991@gmail.com>
2023-12-01 17:24:50 -0300

    cambios en morgan

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const articulosRoutes = require('./router/articulos.routes');
 const cors = require('cors');
 
 //Configuraciones globales
-app.use(morgan('common'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
