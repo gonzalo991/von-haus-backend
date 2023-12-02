e4ceb5ff4274351f757aa4a1b799a2fbd76f3344
Gonzalo Araya <gonxaraya991@gmail.com>
2023-12-02 20:55:21 -0300

    cambiamos post por put en edit controller 3

--- src/router/articulos.routes.js
+++ src/router/articulos.routes.js
@@ -7,7 +7,7 @@ const upload = require('../middlewares/multer.middleware');
 route.get('/getArticles', Controller.getArticle);
 route.get('/:id', Controller.getArticleById);
 route.post('/addArticle', Authentication, upload.single('image'), Controller.addArticle);
-route.put('/editArticle/:id', Authentication, Controller.editArticle);
+route.post('/editArticle/:id', Authentication, Controller.editArticle);
 route.delete('/deleteArticle/:id', Authentication, Controller.deleteArticle);
 
 module.exports = route;
